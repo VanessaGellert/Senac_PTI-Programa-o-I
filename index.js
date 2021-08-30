bdb7cb8ff6758573ff9b28746a2dd987a8b6fc20
Vanessa Gellert <nessagellert@gmail.com>
2021-08-30 23:09:54 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+do {
+  var gasolina = parseFloat(prompt('Digite o valor da Galosina'));
+
+  var alcool = parseFloat(prompt('Digite o valor do Álcool'));
+
+  var media = alcool/gasolina
+
+
+  if (media == 0.7){
+    alert ('Abasteça com Álcool ou Gasolina')
+  }
+  else if(media >= 0.7){
+    alert ('Abasteça com Gasolina.')
+  }
+  else if (media <= 0.7){
+    alert ('Abasteça com Álcool.')
+  }
+
+  var continuar = (prompt('Deseja repetir o cálculo?\n s ou n'));
+
+} while (continuar == 's' )
